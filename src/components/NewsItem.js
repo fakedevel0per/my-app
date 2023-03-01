5d51d2bb5114bb965b9aafc71d3587f5f8ed085e
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-02 01:06:42 +0530

    fetching data through API

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,10 +7,10 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl} className="card-img-top" alt="..."/>
+          <img src={imageUrl?imageUrl:"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/CE13/production/_128755725_brendonmccullumandbenstokes.jpg"} className="card-img-top" alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">{title}...</h5>
-              <p className="card-text">{description}...</p>
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{description}</p>
               <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
