3b16829bc64ac54e92a1a63c0ff5ce3722457f81
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-03 14:21:32 +0530

    prev and next button, added loading

--- src/components/News.js
+++ src/components/News.js
@@ -1,35 +1,78 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
-
+import Spinner from './Spinner'
 export class News extends Component {
 constructor(){
   super();
   this.state = {
-      articles: this.articles,
       articles: [],
-      loading: false
+      loading: false,
+      page: 1
   }
 }
 async componentDidMount(){
-  let url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=a288e18d3c1b4ee5844e59692e0c8a1e";
+  let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=a288e18d3c1b4ee5844e59692e0c8a1e&page=1&pageSize=${this.props.pageSize}`;
+  this.setState({loading: true});
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  console.log(parsedData);
+  this.setState({
+    articles: parsedData.articles,
+    totalResults: parsedData.totalResults,
+    loading: false
+  });
+}
+
+handlePrevClick = async ()=>{
+  console.log("previous");
+  let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=a288e18d3c1b4ee5844e59692e0c8a1e&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+  this.setState({loading: true});
   let data = await fetch(url);
   let parsedData = await data.json();
   console.log(parsedData);
   this.setState({articles: parsedData.articles});
+
+  this.setState({
+    page: this.state.page - 1,
+    loading: false
+  })
+}
+handleNextClick = async ()=>{
+  console.log(this.state.totalResults/this.props.pageSize);
+if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+ 
+  let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=a288e18d3c1b4ee5844e59692e0c8a1e&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
+  this.setState({loading: true});
+  let data = await fetch(url);
+  let parsedData = await data.json();
+  console.log(parsedData);
+  this.setState({articles: parsedData.articles});
+  
+  this.setState({
+    page: this.state.page + 1,
+    loading: false
+  })
+}else{
+
+}
 }
 
   render() {
     return (
       <div className="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
+        <h1 className="text-center">NewsMonkey - Top Headlines</h1>
+        {this.state.loading && <Spinner/>}
         <div className="row">
-        {this.state.articles.map((element)=>{
+        {!this.state.loading && this.state.articles.map((element)=>{
           return <div className="col-md-4" key={element.url}>
             <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
           </div>
         })}
         </div>
-        
+        <div className="container d-flex justify-content-between">
+            <button disabled={this.state.page<=1} type="button" class="btn btn-primary" onClick={this.handlePrevClick}> &larr; Previous</button>
+            <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" class="btn btn-primary" onClick={this.handleNextClick}>Next &rarr;</button>
+        </div>
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,11 +7,11 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{width: "18rem"}}>
-          <img src={imageUrl?imageUrl:"https://ichef.bbci.co.uk/live-experience/cps/624/cpsprodpb/CE13/production/_128755725_brendonmccullumandbenstokes.jpg"} className="card-img-top" alt="..."/>
+          <img src={imageUrl?imageUrl:"https://twt-thumbs.washtimes.com/media/image/2018/11/06/2018_house_barr_kentucky_05998_c0-182-4196-2629_s1200x700.jpg?81c3647d696d25f39bee9228840afc9f81bac7d5"} className="card-img-top" alt="..."/>
             <div className="card-body">
               <h5 className="card-title">{title}</h5>
               <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read more</a>
+              <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read more</a>
             </div>
         </div>
       </div>
