1e6a08f7e66047a9e3fb20cb0ed0f5109090bdd8
Pratik Gaikwad <pratik2000gaikwad@gmail.com>
2023-03-05 14:02:46 +0530

    refactored

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
- 
+
   render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date, source } = this.props;
     return (
       <div className="my-3">
         <div className="card">
-          <img src={imageUrl?imageUrl:"https://twt-thumbs.washtimes.com/media/image/2018/11/06/2018_house_barr_kentucky_05998_c0-182-4196-2629_s1200x700.jpg?81c3647d696d25f39bee9228840afc9f81bac7d5"} className="card-img-top" alt="..."/>
-            <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
-              <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read more</a>
-            </div>
+          <img src={imageUrl ? imageUrl : "https://upload.wikimedia.org/wikipedia/commons/9/9b/Google_news_logo.png?20210712160907"} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">{title} <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{ left: '85%', zIndex: '1' }}>{source}</span></h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-muted">By {author ? author : "Unknown"} on {new Date(date).toGMTString()}</small></p>
+            <a href={newsUrl} target="_blank" rel="noopener noreferrer" className="btn btn-sm btn-primary">Read more</a>
+          </div>
         </div>
       </div>
     )
